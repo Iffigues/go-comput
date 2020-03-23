cdc0f9ddccad7bdce6b9c8394fc07b558ab142b7
iiffigues <iffigues@airdeiiffigues.home>
2020-03-23 14:29:28 +0100

    coding refacto

--- op.go
+++ op.go
@@ -10,15 +10,18 @@ func (r *obj) getPoly(a int) (b float64) {
 }
 
 func (r *obj) zero() {
+	r.print()
 	r.getPoly(0)
 }
 
 func (r *obj) one() {
+	r.print()
 	r.getPoly(0)
 	r.getPoly(1)
 }
 
 func (r *obj) two() {
+	r.print()
 	r.getPoly(0)
 	r.getPoly(1)
 	r.getPoly(2)
